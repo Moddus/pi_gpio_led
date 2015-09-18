83ba05f06be53803bd8ff49edde13717b5edf66a
Markus Boese <markus.boese@gmail.com>
2015-09-18 15:47:57 +0200

    init.

---
+++ src/main.c
@@ -0,0 +1,37 @@
+#include <wiringPi.h>
+#include <stdio.h>
+#include <stdlib.h>
+
+#define GPIO_PIN 29 /* Pin 40 / GPIO 21 */
+#define TRUE 1
+#define FALE 0
+#define ON TRUE
+#define OFF FALSE
+
+void wiringpi_init() {
+	if (wiringPiSetup() == -1)
+		exit(EXIT_FAILURE);
+	
+	pinMode(PIN, OUTPUT);
+}
+
+void wiringpi_led_on() {
+	digitalWrite(GPIO_PIN, ON);
+}
+
+void wiringpi_led_off() {
+	digitalWrite(GPIO_PIN, OFF);
+}
+
+void wiringpi_delay(long t) {
+	delay(t);
+}
+
+int main() {
+	wiringpi_init();
+	while(1) {
+		wiringpi_led_on();		
+		wiringpi_delay(500);
+		wiringpi_led_off();
+	}
+}
